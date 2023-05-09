14da79d03e2cf527811e4e3567f6cfd6a31cce33
Krishalika.R <krishalika.r@synergenhealth.com>
2023-05-09 10:02:48 +0530

    initial code to discover connected devices

--- app/src/main/java/com/example/cse/syncmate/FolderAdapter.java
+++ app/src/main/java/com/example/cse/syncmate/FolderAdapter.java
@@ -1,6 +1,7 @@
 package com.example.cse.syncmate;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +20,7 @@ public class FolderAdapter extends ArrayAdapter<String> {
 
     private int iconResource1;
     private int iconResource2;
+
     public FolderAdapter(Context context, List<String> items, int iconResource1, int iconResource2) {
         super(context, 0, items);
         this.iconResource1 = iconResource1;
@@ -27,7 +30,7 @@ public class FolderAdapter extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
-        if (view == null){
+        if (view == null) {
             view = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
         ImageView folderImage = view.findViewById(R.id.folderImage);
@@ -38,6 +41,22 @@ public class FolderAdapter extends ArrayAdapter<String> {
 
         ImageButton syncImage = view.findViewById(R.id.sync_btn);
         syncImage.setImageResource(iconResource2);
+
+        syncImage.setOnClickListener(view1 -> {
+            Log.d("KRISH8", "STARTED CLICK METHOD");
+            WifiDeviceScanner wifiDeviceScanner = new WifiDeviceScanner(getContext());
+            Log.d("KRISH9", "PASSED INITIALIZATION");
+
+            List<String> eligibleDevices = wifiDeviceScanner.scanForDevices();
+            if (eligibleDevices.isEmpty()) {
+                Log.d("KRISH10 ELIGIBILE LIST SIZE", String.valueOf(eligibleDevices.size()));
+                Toast.makeText(getContext(), "NO DEVICES FOUND", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d("KRISH16 NO ELIGIBILE LIST", String.valueOf(eligibleDevices.size()) + eligibleDevices);
+                Toast.makeText(getContext(), "Eligible devices: " + eligibleDevices, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return view;
     }
